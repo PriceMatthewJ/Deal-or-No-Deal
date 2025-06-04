12b8884ffb4c4b135c67c0dd8edd3edf0b561804
PriceMatthewJ <inaway@gmail.com>
2025-06-04 11:23:53 -0600

    Add demo with Vite

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DealOrNoDealGame from './DealOrNoDealGame.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DealOrNoDealGame />
+  </React.StrictMode>
+);
